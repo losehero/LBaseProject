edcdcfaf505ab99b4cf0723d2a6bdf75bdd05d28
jinlong <jinlong@medical-lighter.com>
2016-09-27 15:16:34 +0800

    add

---
+++ LBaseProject/LBaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBaseProject
+//
+//  Created by jinlong9 on 16/9/27.
+//  Copyright © 2016年 losehero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBaseProject/LBaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBaseProject
+//
+//  Created by jinlong9 on 16/9/27.
+//  Copyright © 2016年 losehero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
